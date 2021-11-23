662f77ec89393feaa64d3fe960efbb60c08130e7
Weslley Oliveira <weslleyoliveira258@gmail.com>
2021-11-22 21:27:23 -0300

    feat: delete todo user

--- src/index.js
+++ src/index.js
@@ -100,7 +100,18 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+  const { user } = request;
+  const id = request.params.id;
+
+  const todoExists = user.todos.findIndex(todo => todo.id === id);
+
+  if (!todoExists) {
+    return response.status(404).json({error: 'Todo not found.'});
+  }
+  
+  user.todos.splice(todoExists, 1);
+
+  return response.status(200).json() 
+}); 
 
 module.exports = app;
